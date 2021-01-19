4238ebefee9864c50a5c93348816046ef46ea056
Tom Cumming <mail@tomcumming.co.uk>
2021-01-19 21:05:31 +0000

    symbols tests

--- src/symbols.rs
+++ src/symbols.rs
@@ -4,7 +4,7 @@ pub type Symbol = String;
 
 pub type SymbolPowers = BTreeMap<Symbol, usize>;
 
-#[derive(Clone)]
+#[derive(Debug, Clone, PartialEq)]
 pub struct Symbols(pub BTreeMap<SymbolPowers, f32>);
 
 impl Symbols {
@@ -14,7 +14,10 @@ impl Symbols {
             .iter()
             .flat_map(|(lhs_power, lhs_scale)| {
                 rhs_powers.iter().map(move |(rhs_power, rhs_scale)| {
-                    (mult_powers(lhs_power, &rhs_power), lhs_scale * rhs_scale)
+                    (
+                        mult_symbol_powers(lhs_power, &rhs_power),
+                        lhs_scale * rhs_scale,
+                    )
                 })
             })
             .fold(Symbols(BTreeMap::new()), |prev, (pwr, scale)| {
@@ -47,34 +50,72 @@ impl Symbols {
     }
 }
 
-fn add_symbol_powers(lhs: SymbolPowers, rhs: SymbolPowers) -> SymbolPowers {
-    let mut result: SymbolPowers = BTreeMap::new();
-
-    for (sym, pow) in lhs.into_iter().chain(rhs.into_iter()) {
-        let existing = *result.get(&sym).unwrap_or(&0);
-        result.insert(sym, existing + pow);
-    }
-
-    result
+fn mult_symbol_powers(lhs: &SymbolPowers, rhs: &SymbolPowers) -> SymbolPowers {
+    lhs.iter()
+        .chain(rhs.iter())
+        .fold(BTreeMap::new(), |mut prev, (sym, pow)| {
+            let existing = prev.remove(sym).unwrap_or(0);
+            prev.insert(sym.to_string(), existing + pow);
+            prev
+        })
 }
 
-fn mult_powers(lhs: &SymbolPowers, rhs: &SymbolPowers) -> SymbolPowers {
-    lhs.iter()
-        .flat_map(|(lhs_sym, lhs_pow)| {
-            rhs.iter().map(move |(rhs_sym, rhs_pow)| {
-                if lhs_sym == rhs_sym {
-                    vec![(lhs_sym.to_string(), lhs_pow + rhs_pow)]
-                        .into_iter()
-                        .collect()
-                } else {
-                    vec![
-                        (lhs_sym.to_string(), *lhs_pow),
-                        (rhs_sym.to_string(), *rhs_pow),
-                    ]
-                    .into_iter()
-                    .collect()
-                }
-            })
-        })
-        .fold(BTreeMap::new(), add_symbol_powers)
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_simple_mult_symbol_powers() {
+        let lhs: SymbolPowers = vec![("x".to_string(), 2), ("y".to_string(), 3)]
+            .into_iter()
+            .collect();
+        let rhs: SymbolPowers = vec![("y".to_string(), 4), ("z".to_string(), 5)]
+            .into_iter()
+            .collect();
+
+        assert_eq!(
+            mult_symbol_powers(&lhs, &rhs),
+            vec![
+                ("x".to_string(), 2),
+                ("y".to_string(), 3 + 4),
+                ("z".to_string(), 5)
+            ]
+            .into_iter()
+            .collect()
+        );
+    }
+
+    #[test]
+    fn test_mult_power_increase() {
+        // (2 + x) * (3 + xx) = 6 + 2xx + 3x + xxx
+        let lhs = Symbols(
+            vec![
+                (vec![("x".to_string(), 1)].into_iter().collect(), 1.0),
+                (BTreeMap::new(), 2.0),
+            ]
+            .into_iter()
+            .collect(),
+        );
+        let rhs = Symbols(
+            vec![
+                (vec![("x".to_string(), 2)].into_iter().collect(), 1.0),
+                (BTreeMap::new(), 3.0),
+            ]
+            .into_iter()
+            .collect(),
+        );
+
+        let expected = Symbols(
+            vec![
+                (vec![("x".to_string(), 3)].into_iter().collect(), 1.0),
+                (vec![("x".to_string(), 2)].into_iter().collect(), 2.0),
+                (vec![("x".to_string(), 1)].into_iter().collect(), 3.0),
+                (BTreeMap::new(), 6.0),
+            ]
+            .into_iter()
+            .collect(),
+        );
+
+        assert_eq!(lhs.mult(&rhs), expected);
+    }
 }
