9fb91dd5acd1720a83d1b258a8dd12e5397b0f49
Tom Cumming <mail@tomcumming.co.uk>
2021-01-21 20:52:52 +0000

    Parse parenthesis

--- macro-ga-logic/src/parser.rs
+++ macro-ga-logic/src/parser.rs
@@ -2,7 +2,7 @@ use std::iter::Peekable;
 use std::str::FromStr;
 
 use proc_macro2::token_stream::{self};
-use proc_macro2::{TokenStream, TokenTree};
+use proc_macro2::{Delimiter, TokenStream, TokenTree};
 
 use crate::expr::Expr;
 
@@ -18,22 +18,44 @@ impl Parser {
     }
 }
 
+#[derive(Debug, PartialEq)]
+pub enum ParsedExpr {
+    Brackets(Expr),
+    Naked(Expr),
+}
+
+impl From<ParsedExpr> for Expr {
+    fn from(pe: ParsedExpr) -> Expr {
+        match pe {
+            ParsedExpr::Brackets(e) => e,
+            ParsedExpr::Naked(e) => e,
+        }
+    }
+}
+
 impl Parser {
-    pub fn parse_operand(&mut self) -> Result<Expr, String> {
+    pub fn parse_operand(&mut self) -> Result<ParsedExpr, String> {
         let next_token = self.tokens.next().ok_or("Unexpected end of expression")?;
 
         match next_token {
-            TokenTree::Literal(l) => parse_constant(l.to_string()),
-            TokenTree::Ident(i) => Ok(parse_ident(i.to_string())),
+            TokenTree::Literal(l) => parse_constant(l.to_string()).map(ParsedExpr::Naked),
+            TokenTree::Ident(i) => Ok(ParsedExpr::Naked(parse_ident(i.to_string()))),
             TokenTree::Punct(p) if p.as_char() == '-' => {
                 let e = self.parse_operand()?;
-                Ok(Expr::Mul(Box::new(Expr::Constant(-1.0)), Box::new(e)))
+                Ok(ParsedExpr::Naked(Expr::Mul(
+                    Box::new(Expr::Constant(-1.0)),
+                    Box::new(e.into()),
+                )))
+            }
+            TokenTree::Group(g) if g.delimiter() == Delimiter::Parenthesis => {
+                let mut sub_parser = Parser::from_tokens(g.stream());
+                Ok(ParsedExpr::Brackets(sub_parser.parse_expression()?.into()))
             }
             token => Err(format!("Unexpected token in operand '{}'", token)),
         }
     }
 
-    pub fn parse_expression(&mut self) -> Result<Expr, String> {
+    pub fn parse_expression(&mut self) -> Result<ParsedExpr, String> {
         let lhs = self.parse_operand()?;
 
         match self.tokens.peek() {
@@ -41,8 +63,11 @@ impl Parser {
             Some(token) => match token {
                 TokenTree::Punct(p) if p.as_char() == '+' => {
                     self.tokens.next(); // skip the + token
-                    let rhs = self.parse_expression()?;
-                    Ok(Expr::Add(Box::new(lhs), Box::new(rhs)))
+                    let rhs: Expr = self.parse_expression()?.into();
+                    Ok(ParsedExpr::Naked(Expr::Add(
+                        Box::new(lhs.into()),
+                        Box::new(rhs),
+                    )))
                 }
                 token => Err(format!("Unexpected token in expression '{}'", token)),
             },
@@ -78,7 +103,7 @@ mod tests {
         let mut parser = Parser::from_tokens(TokenStream::from_str("1.23 123").unwrap());
 
         for expected in [1.23, 123.0].iter() {
-            match parser.parse_operand()? {
+            match parser.parse_operand()?.into() {
                 Expr::Constant(c) => {
                     assert_eq!(c, *expected);
                 }
@@ -94,7 +119,7 @@ mod tests {
         let mut parser = Parser::from_tokens(TokenStream::from_str("你好   World").unwrap());
 
         for expected in ["你好", "World"].iter() {
-            match parser.parse_operand().unwrap() {
+            match parser.parse_operand().unwrap().into() {
                 Expr::Symbol(s) => {
                     assert_eq!(s.as_str(), *expected);
                 }
@@ -107,7 +132,7 @@ mod tests {
     fn test_parse_negated_number() {
         let mut parser = Parser::from_tokens(TokenStream::from_str("-123").unwrap());
 
-        let e = parser.parse_operand().unwrap();
+        let e: Expr = parser.parse_operand().unwrap().into();
         assert_eq!(
             e,
             Expr::Mul(
@@ -121,7 +146,7 @@ mod tests {
     fn test_parse_simple_addition() {
         let mut parser = Parser::from_tokens(TokenStream::from_str("1 + 2 +   3").unwrap());
 
-        let e = parser.parse_expression().unwrap();
+        let e: Expr = parser.parse_expression().unwrap().into();
         assert_eq!(
             e,
             Expr::Add(
@@ -130,6 +155,23 @@ mod tests {
                     Box::new(Expr::Constant(2.0)),
                     Box::new(Expr::Constant(3.0))
                 ))
+            )
+        );
+    }
+
+    #[test]
+    fn test_parse_brackets() {
+        let mut parser = Parser::from_tokens(TokenStream::from_str("(1 + 2) + 3").unwrap());
+
+        let e: Expr = parser.parse_expression().unwrap().into();
+        assert_eq!(
+            e,
+            Expr::Add(
+                Box::new(Expr::Add(
+                    Box::new(Expr::Constant(1.0)),
+                    Box::new(Expr::Constant(2.0))
+                )),
+                Box::new(Expr::Constant(3.0))
             )
         );
     }
